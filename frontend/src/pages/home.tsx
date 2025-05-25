d8f1270a91fcfe4b4ea4c90a1721489221991fda
김첨지 <gangeunno@gmail.com>
2025-05-25 23:43:20 +0900

    2025.05.25 프론트엔드 css 추가

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,18 @@
+// src/pages/Home.tsx
+
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.hero}>
+        <h1 className={styles.title}>Welcome to Bikini Bottom Box Office</h1>
+        <p className={styles.subtitle}>Reserve Your Coral Seat Today</p>
+        <Link to="/showList" className={styles.cta}>🎫 예매하러 가기</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
