382de170e10353a5d5081da53e39c4232a528498
김첨지 <gangeunno@gmail.com>
2025-05-25 23:40:33 +0900

    2025.05.25 프론트엔드 라우트 추가, 디비 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,32 @@
 // src/App.tsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
+import Home from './pages/home';
 import Login from './pages/account/Login';
 import Signup from './pages/account/Signup';
 import ShowList from './pages/show/showList';
 import ShowDetail from './pages/show/showDetail';
+import SeatSelector from './pages/reserve/seatSelector';
+import Payment from './pages/payment/Payment';
+import PaymentSuccess from './pages/payment/paymentSuccess';
 import './styles/colors.css'
+
 function App() {
   return (
+    
     <BrowserRouter>
       <Routes>
+          <Route index element={<Home />} />
         <Route path="/" element={<Layout />}>
-          <Route index element={<Login />} />
+
+          <Route  path="login"  element={<Login />} />
           <Route path="signup" element={<Signup />} />
+          
           <Route path="showList" element={<ShowList />} />
-          <Route path="/shows/:id" element={<ShowDetail />} />
+          <Route path="shows/:id" element={<ShowDetail />} />
+          <Route path="shows/:id/seatSelector" element={<SeatSelector/>} />
+          <Route path="payment" element={<Payment/>} />
+          <Route path="payment/success" element={<PaymentSuccess />} />
         </Route>
       </Routes>
     </BrowserRouter>
